54c17a4573aa9cc470f46a2065dbfce3ebb399dd
Ilya Epifanov <ilya.epifanov@datravel.com>
2015-08-11 16:38:00 +0300

    fixed startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$1" == 'zookeeper-server' ] || [ "$1" == 'zookeeper-server-initialize' ]; then
-        mkdir -m a=rx,u=rwx /var/run/zookeeper
+        mkdir -p -m a=rx,u=rwx /var/run/zookeeper
 	chown -R zookeeper /var/{lib,run,log}/zookeeper
 	set -- gosu zookeeper "$@"
 fi
